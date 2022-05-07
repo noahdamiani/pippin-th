3df2270857faaa56c5490d8f66307b22c89e745d
Noah Damiani <noahdamiani@gmail.com>
2022-05-06 20:24:47 -0500

    Remove boilerplate endpoint, add in /titles

---
+++ pages/api/titles.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Title } from "schema/title";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Title[]>
+) {
+  res.status(200).json([
+    {
+      id: 1,
+      orderNumber: "PPN000001",
+      searchEffectiveDate: "2/21/22",
+      address: "1350 Shores Cir, Rockwall, TX 75087",
+      image:
+        "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 ",
+      status: "done",
+    },
+    {
+      id: 2,
+      orderNumber: "PPN000002",
+      searchEffectiveDate: "2/22/22",
+      address: "2250 Belfry Ct, Rockwall, TX 75087",
+      image:
+        "https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 ",
+      status: "todo",
+    },
+    {
+      id: 3,
+      orderNumber: "PPN000003",
+      searchEffectiveDate: "2/23/22",
+      address: "1923 Greenville Ave, Dallas, TX 75206",
+      image:
+        "https://media.istockphoto.com/photos/single-family-new-construction-homein-suburb-neighborhood-in-the-picture-id1147674294?k=20&m=1147674294&s=612x612&w=0&h=7eSceNNA1USRkMbTM6DXPl7UGB1kRvHHcdBtehn4SQ=",
+      status: "todo",
+    },
+  ]);
+}
---
+++ schema/title.ts
@@ -0,0 +1,8 @@
+export interface Title {
+  id: number;
+  orderNumber: string;
+  searchEffectiveDate: string;
+  address: string;
+  image: string;
+  status: string;
+}
